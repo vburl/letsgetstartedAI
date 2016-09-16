7eeb53f926ef842614057cf64e8d55dad34f4508
vburl <vburl@iscte-iul.pt>
2016-09-17 00:32:44 +0100

    synaptic js NN

---
+++ synaptic/src/init.js
@@ -0,0 +1,32 @@
+const synaptic = require('./synaptic');
+const mnist = require('mnist');
+
+const set = mnist.set(700, 20);
+
+const trainingSet = set.training;
+const testSet = set.test;
+const Layer = synaptic.Layer;
+const Network = synaptic.Network;
+const Trainer = synaptic.Trainer;
+
+const inputLayer = new Layer(784);
+const hiddenLayer = new Layer(100);
+const outputLayer = new Layer(10);
+
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+const myNetwork = new Network({
+    input: inputLayer,
+    hidden: [hiddenLayer],
+    output: outputLayer
+});
+const trainer = new Trainer(myNetwork);
+trainer.train(trainingSet, {
+    rate: .2,
+    iterations: 20,
+    error: .1,
+    shuffle: true,
+    log: 1,
+    cost: Trainer.cost.CROSS_ENTROPY
+});
